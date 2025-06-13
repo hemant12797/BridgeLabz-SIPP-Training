05666279a7b49ec0f15ad6d5cbc3f273ebc1c430
hemant12797 <hemantthakur8439@gmail.com>
2025-06-13 09:41:35 +0530

    Day1

---
+++ Day1_PracticeProblems/Add_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Add_two_numbers {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner (System.in)) {
+			int a =sc.nextInt();
+			int b=sc.nextInt();
+			int c =a+b;
+			System.out.print(c);
+		}
+	}
+
+}
---
+++ Day1_PracticeProblems/Kilometers_to_miles.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Kilometers_to_miles {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double k=sc.nextDouble();
+		System.out.print(k*0.621371);
+	}
+
+}
