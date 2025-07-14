495368c82a6b22fbd62d69b372d39a72f4a53097
hemant12797 <hemantthakur8439@gmail.com>
2025-07-14 11:14:39 +0530

    Create SortStackRecursively.java

---
+++ Stack & Queue/StackQueue/SortStackRecursively.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class SortStackRecursively {
+    public static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertInSortedOrder(stack, temp);
+        }
+    }
+
+    private static void insertInSortedOrder(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
+            stack.push(element);
+        } else {
+            int temp = stack.pop();
+            insertInSortedOrder(stack, element);
+            stack.push(temp);
+        }
+    }
+}
